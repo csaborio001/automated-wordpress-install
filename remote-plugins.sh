035f87988c868e98cd0f872c0f4010042f71c444
csaborio <christian@saborio.org>
2019-09-19 13:33:56 +1000

    Removed optional plugins

--- remote-plugins.sh
+++
@@ -1,8 +0,0 @@
-### --- REMOTE PLUGINS --- ###
-echo "Installing remote plugins..."
-
-# Include the plugins you wish to install, for example:
-
-# Install and Activate wp-cerber, changes the login url for the website
-# wp plugin install wp-cerber --activate
-
